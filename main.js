ffda288e85cbf949eba778ec37d1377f73e92755
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-23 02:05:08 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// Masala: oy raqami berilgan kiritilgan oyda necha kun borligini aniqlang
+
+let n=4;
+
+switch(n){
+    case (n=1):
+        console.log("31 kun");
+        break;
+
+    case (n=2):
+        console.log("18 kun");
+        break;
+
+    case (n=3):
+        console.log("31 kun");
+        break;
+
+    case (n=4):
+        console.log("30 kun");
+        break;
+
+    case (n=5):
+        console.log("31 kun");
+        break;
+
+    case (n=6):
+        console.log("30 kun");
+        break;
+
+    case (n=7):
+        console.log("31 kun");
+        break;
+
+    case (n=8):
+        console.log("31 kun");
+        break;
+
+    case (n=9):
+        console.log("30 kun");
+        break;
+
+    case (n=10):
+        console.log("31 kun");
+        break;
+
+    case (n=11):
+        console.log("30 kun");
+        break;
+
+    case (n=12):
+        console.log("31 kun");
+        break;
+    
+}
